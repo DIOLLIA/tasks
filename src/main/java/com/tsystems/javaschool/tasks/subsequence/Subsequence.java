c322c156d04db4179fecb594b66116c1169b010b
diollia <diollia88@gmail.com>
2018-01-24 00:51:16 +0300

    two methods for one purpose

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -1,5 +1,6 @@
 package com.tsystems.javaschool.tasks.subsequence;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Subsequence {
@@ -8,13 +9,61 @@ public class Subsequence {
      * Checks if it is possible to get a sequence which is equal to the first
      * one by removing some elements from the second one.
      *
-     * @param x first sequence
-     * @param y second sequence
+     * @param subList first sequence
+     * @param list    second sequence
      * @return <code>true</code> if possible, otherwise <code>false</code>
      */
-    @SuppressWarnings("rawtypes")
-    public boolean find(List x, List y) {
-        // TODO: Implement the logic here
-        return false;
+    public boolean find2(List subList, List list) {
+        if (subList == null || list == null) {
+            throw new IllegalArgumentException("One of list contains null");
+        }
+        if (subList.size() > list.size()) {
+            return false;
+        }
+        if (subList.isEmpty()) {
+            return true;
+        }
+
+        boolean isPossible = false;
+
+        int j = 0;
+        for (int i = 0; i < subList.size(); i++) {
+            for (; j < list.size(); j++) {
+                if (list.get(j).equals(subList.get(i))) {
+                    if (i == subList.size() - 1) {
+                        isPossible = true;
+                    }
+                    j++;
+                    break;
+                }
+            }
+        }
+        return isPossible;
+    }
+
+    //Этот метод так же рабочий. Мне он нравится больше, так как записан более компактно
+    public boolean find(List subList, List list) {
+        if (subList == null || list == null) {
+            throw new IllegalArgumentException("One of list contains null");
+        }
+        if (subList.size() > list.size()) {
+            return false;
+        }
+        if (subList.isEmpty()) {
+            return true;
+        }
+        Iterator iterForList = list.iterator();
+        for (Object elementOfSubList : subList) {
+            if (iterForList.hasNext()) {
+                while (iterForList.hasNext()) {
+                    if (elementOfSubList.equals(iterForList.next())) {
+                        break;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
